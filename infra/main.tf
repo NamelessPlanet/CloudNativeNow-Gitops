6d96c09afdbb8caceed00357a784fb32d134b771
Marcus Noble <github@marcusnoble.co.uk>
2024-12-24 09:33:26 +0000

    Format
    Signed-off-by: Marcus Noble <github@marcusnoble.co.uk>

--- infra/main.tf
+++ infra/main.tf
@@ -3,30 +3,30 @@ resource "civo_network" "cloudnativenow" {
 }
 
 resource "civo_firewall" "cloudnativenow" {
-  name = "cloudnativenow-firewall"
+  name                 = "cloudnativenow-firewall"
   create_default_rules = true
-  network_id = civo_network.cloudnativenow.id
+  network_id           = civo_network.cloudnativenow.id
 }
 
 resource "civo_kubernetes_cluster" "cloudnativenow" {
-  name = "CloudNative.now"
+  name        = "CloudNative.now"
   firewall_id = civo_firewall.cloudnativenow.id
-  network_id = civo_network.cloudnativenow.id
+  network_id  = civo_network.cloudnativenow.id
 
-  cluster_type = "k3s"
+  cluster_type       = "k3s"
   kubernetes_version = "1.30.5-k3s1"
-  cni = "cilium"
+  cni                = "cilium"
 
   write_kubeconfig = true
 
   pools {
-    size = "g4s.kube.xsmall"
+    size       = "g4s.kube.xsmall"
     node_count = 2
   }
 }
 
 resource "civo_object_store" "cloudnativenow" {
-  name = "cloudnativenow"
+  name        = "cloudnativenow"
   max_size_gb = 500
 }
 
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,26 +1,26 @@
 output "kubernetes_name" {
-  value = civo_kubernetes_cluster.cloudnativenow.name
+  value       = civo_kubernetes_cluster.cloudnativenow.name
   description = "The randomly generated name for this cluster"
 }
 
 output "kubernetes_api_endpoint" {
-  value = civo_kubernetes_cluster.cloudnativenow.api_endpoint
+  value       = civo_kubernetes_cluster.cloudnativenow.api_endpoint
   description = "The API endpoint of the Kubernetes cluster"
 }
 
 output "kubernetes_kubeconfig" {
-  value = civo_kubernetes_cluster.cloudnativenow.kubeconfig
+  value       = civo_kubernetes_cluster.cloudnativenow.kubeconfig
   description = "The KubeConfig for the Kubernetes cluster"
-  sensitive = true
+  sensitive   = true
 }
 
 output "objectstore_url" {
-  value = civo_object_store.cloudnativenow.bucket_url
+  value       = civo_object_store.cloudnativenow.bucket_url
   description = "The URL for the objectstore"
 }
 
 output "objectstore_access_key_id" {
-  value = civo_object_store.cloudnativenow.access_key_id
+  value       = civo_object_store.cloudnativenow.access_key_id
   description = "The Access Key ID for the objectstore"
-  sensitive = true
+  sensitive   = true
 }
